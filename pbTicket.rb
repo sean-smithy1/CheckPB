3966d550150d2c0a33d7bcff05b90610683ac432
Sean Smith <sean.smithy1@gmail.com>
2013-10-10 20:12:26 +1000

    Parser for tickets created

--- pbTicket.rb
+++ pbTicket.rb
@@ -1,2 +1,16 @@
 #!/usr/bin/ruby
-Ticket
+
+ticket = {}
+
+File.open("./Numbers.txt").each do |line|
+  if (line =~ /^Number: (.*)/)
+    @id=$1.strip.to_s
+  end
+
+  if (line =~ /^\[(\d*)\]\s(.*)\s\sPB: (.*)$/)
+    @game=$1.strip.to_i
+    @nums=$2.split(" ").map { |s| s.to_i}
+    @nums << $3.strip.to_i if $3 != 'POWERHIT'
+    puts "#{@id}, #{@game}, #{@nums}"
+  end
+end
